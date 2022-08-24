29f5727cf6ef9d7bc00c362d9d4df2236366675f
Abdulsamad02 <107322324+Abdulsamad02@users.noreply.github.com>
2022-08-24 04:06:36 -0700

    Update counter-app.js

--- counter-app.js
+++ counter-app.js
@@ -1,4 +1,48 @@
-A cool counter app
+import  {useState} from 'react';
+import  './styles.css';
+
+function App() {
+  const [value, setValue] = useState(1)
+   
+
+  function increament () {
+    setValue(value +1)
+  };
+  function decreament () {
+    setValue (value -1)
+    
+     
+  
+  };
+
+  function reset() {
+    setValue(0)
+    
+  };
 
 
-Thank you
+  return (
+    <div>
+      <h1 className='header'>A Simple Counter App</h1>
+      <h2>COUNT STARTS</h2>
+      <p className='count'>{value}</p>
+     
+      <button className='btn' onClick={increament} 
+        
+      disabled={value>=10}>increase</button>
+    
+      <button className='btn' onClick={decreament}
+       disabled={value=== 0}>
+       decrease
+       </button>
+      <div>
+        <button className='btn' onClick={reset} disabled={value=== 0}>reset</button>
+        <h2>Thanks Alot</h2>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
+
